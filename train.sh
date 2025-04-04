0c22450636e88e2cdae91d8fb787bbb83e1329fb
vitaondr <vitaondr@fel.cvut.cz>
2025-04-04 19:02:02 +0200

    sh file added

---
+++ train.sh
@@ -0,0 +1,1 @@
+python3.9 train.py --batch 4 --epochs 2 --cfg models/yolov5s_6dpose_bifpn.yaml --hyp configs/hyp.single.yaml --weights ../data/weights/yolov5s.pt --data configs/drone_data/drone.yaml --rect --optimizer Adam 
