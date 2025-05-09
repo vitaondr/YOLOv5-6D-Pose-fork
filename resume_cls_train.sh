17edb34a1c2f9b3ecdd30a07c6bab2b295b090fb
vitaondr <vitaondr@fel.cvut.cz>
2025-05-09 13:43:46 +0200

    changed resume file and num of epochs in real training

--- resume_cls_train.sh
+++ resume_cls_train.sh
@@ -2,15 +2,15 @@
 
 # options preceded with #SBATCH must be before any executable commands in the script
 
-#SBATCH --job-name=yolo6D_train                                			# just to recognize the job in the queue overview
+#SBATCH --job-name=yolo6D_train_resume                               			# just to recognize the job in the queue overview
 #SBATCH --nodes=1
 #SBATCH --ntasks=3                     						# total number of tasks (total number of srun commands)
 #SBATCH --gres=gpu:1		                                    		# generic consumable resources, see https://slurm.schedmd.com/sbatch.html
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=30GB
 #SBATCH --partition=gpulong						                      	# partition name
-#SBATCH --output=YOLOv5-6D-Pose-fork/runs/cls_train/yolo6D_train.out   		    # standard output file
-#SBATCH --error=YOLOv5-6D-Pose-fork/runs/cls_train/yolo6D_train.out    		    # standard error file
+#SBATCH --output=YOLOv5-6D-Pose-fork/runs/cls_train/yolo6D_train_resume.out   		    # standard output file
+#SBATCH --error=YOLOv5-6D-Pose-fork/runs/cls_train/yolo6D_train_resume.out    		    # standard error file
 #SBATCH --time=60:00:00							                        # time limit
 
 PATH_TO_YOLO6D="$HOME/YOLOv5-6D-Pose-fork"
@@ -18,7 +18,7 @@ PATH_TO_YOLO6D_IMG="$HOME/YOLOv5-6D-Pose-fork/singularity.sif"
 
 # COMMAND1="$PATH_TO_YOLO6D/train.sh"
 COMMAND1="$PATH_TO_YOLO6D/resume_train.sh"
-COMMAND1_OUT="$PATH_TO_YOLO6D/runs/cls_train/command1.out"
+COMMAND1_OUT="$PATH_TO_YOLO6D/runs/cls_train/command1_resume.out"
 
 echo "running batch job"
 srun --ntasks=1 --gres=gpu:1 singularity run --nv $PATH_TO_YOLO6D_IMG $COMMAND1 > $COMMAND1_OUT 2>&1 &   # "&" symbol is used to run commands simultaneously
